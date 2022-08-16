af3d4380274e25d5d2dd30b725b4fdd57807933d
Rods <rodolfo.perez01@iest.edu.mx>
2022-08-15 20:39:11 -0500

    Express Files

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+const { request, response } = require("express")
+const express = require("express")
+
+const fs = require("fs")
+
+const fsPromises = require("fs/promises") 
+const app = express()
+
+
+
+app.get("/", (request,response) =>{
+
+    response.write("hola bienvenido a nuestra api Express")
+    response.end()
+})
+
+app.get("/files-callbacks", (request, response)=> {
+    fs.readFile("text1.txt", "utf8", (err, data)=>{
+
+        if (err){
+            response.write(err)
+            response.end()
+        }
+
+        response.write(data)
+        response.end()
+    })
+})
+
+app.get("/files-promises", (request, response)=> {
+    fsPromises.readFile("text1.txt", "utf8")
+
+    .then((data)=>{
+        response.write(data)
+        response.end()
+    })
+
+    .catch((error)=>{
+        response.write(error)
+        response.end()
+    })
+})
+
+
+app.get("/file-awaits-async", async (request, response) =>{
+    try{
+    const file = await fsPromises.readFile("text1.txt", "utf8")
+        response.write(file)
+        response.end()
+}catch(error){
+    response.write(error)
+    response.end()
+}
+
+    
+    
+
+})
+app.listen("8080", ()=>{
+
+    console.log("servidor Escuchando")
+})
